e06408f64259b0b3d6cf0452a28dcef5756c7964
Андрей <sr.andrey.rodionov@gmail.com>
2023-04-02 19:07:03 +0300

    currencies fix

--- util/currency.go
+++ util/currency.go
@@ -4,13 +4,13 @@ package util
 const (
 	USD = "USD"
 	EUR = "EUR"
-	CAD = "CAD"
+	RUB = "RUB"
 )
 
 // IsSupportedCurrency returns true if the currency is supported
 func IsSupportedCurrency(currency string) bool {
 	switch currency {
-	case USD, EUR, CAD:
+	case USD, EUR, RUB:
 		return true
 	}
 	return false
